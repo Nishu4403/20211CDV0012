d05ef4168b0c2c71ee65e69d94f5d4c2e6e3a1b2
Nishu4403 <nishathsulikeri@gmail.com>
2024-05-03 16:01:24 +0530

    updated

--- src/Details.js
+++ src/Details.js
@@ -1,12 +1,24 @@
 import React, {useState, useEffect} from "react";
 import { useParams } from "react-router-dom";
+import axios from "axios";
 
 function Details () {
     const [data, setData] = useState([])
     const {productName} = useParams();
+    useEffect( () => {
+        axios.get('http://localhost:3006/products' + productName)
+        .then(res => setData(res.data))
+        .catch(err => console.log(err));
+    }, [])
     return(
         <div>
-            Details
+            <h3>Details Of The Product</h3>
+            <div>Name: {data.productName}</div><br/>
+            <div>Price: {data.price}</div><br/>
+            <div>Rating: {data.rating}</div><br/>
+            <div>Discount: {data.discount}</div><br/>
+            <div>Availability: {data.availability}</div><br/>
+            
         </div>
     )
 }
